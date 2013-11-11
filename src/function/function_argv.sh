fccfe43fae7e8f2d8d0dfd5ab47e4dd8fac951ee
lili <lili00681@gmail.com>
2013-11-11 03:40:53 -0800

    添加函数库，数组

--- src/function/function_argv.sh
+++ src/function/function_argv.sh
@@ -3,12 +3,13 @@
 # Creat Time :Sat 09 Nov 2013 03:57:07 AM GMT
 # Autoor     : lili
 
-
 function test_argv
 {
+	local   LOCAL_value="This is local value.\n"
 	if [ $# -eq 0 ] || [ $# -ge 3 ]
 	then
-		echo $Global_value
+		echo -e $Global_value
+		echo -e $LOCAL_value
 		echo "The function argv is :"
 		echo
 		echo $1
@@ -20,7 +21,7 @@ function test_argv
 	fi
 }
 
-Global_value="lili"
+Global_value="This is global value.\n"
 
 if [ $# -eq 3 ]
 then
@@ -28,4 +29,5 @@ then
 	echo $gaga
 fi
 gaga=`test_argv test1 test2 test3`
+test_argv test1 test2 test3
 echo $gaga
---
+++ src/function/function_array_source.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Creat Time :Mon 11 Nov 2013 02:51:11 AM GMT
+# Autoor     : lili
+function ECHO_lili
+{
+	echo "This is function lili."
+}
+
+function ECHO_cici
+{
+	echo "This is function cici."
+}
