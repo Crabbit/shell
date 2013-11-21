14ff302ee60f0df68935a3b578c154782e72e799
lili <lili00681@gmail.com>
2013-11-20 23:37:26 -0800

    关于显示PATH的几种方法

--- src/sed_awk/regex/pra_echo_path.sh
+++ src/sed_awk/regex/pra_echo_path.sh
@@ -5,13 +5,27 @@
 
 # 实现显示各个path目录
 
+# -------------------------------------------------
 echo "__________sed__________"
 for pathname in $(echo $PATH | sed 's/:/ /g')
 do
 	echo $pathname
 done
-echo
 
+# -------------------------------------------------
+echo "__________cut__________"
+count=1
+while [ $pathname ]
+do
+	pathname=`echo $PATH | cut -d : -f $count`
+	if [ $pathname ]
+	then
+		echo $pathname
+	else
+		break
+	fi
+	count=$[ $count + 1 ]
+done
 # -------------------------------------------------
 echo "__________IFS__________"
 OLD_IFS=$IFS
@@ -21,16 +35,20 @@ do
 	echo $pathname
 done
 IFS=$OLD_IFS
-echo
 
 # -------------------------------------------------
 echo "_______awk__V1_________"
 count=1
-pathname="test"
-pathname=`echo $PATH | awk -F: '{print $count}'`
-echo $pathname
-while [ $pathname -z 0 ]
+pathname=`echo $PATH | awk '{gsub(/:/," ")}{print $0}'`
+for pathname in $pathname
 do
-	pathname=`echo $PATH | awk -F: '{print $count}'`
 	echo $pathname
 done
+# -------------------------------------------------
+echo "_______awk__V2_________"
+	echo $PATH | awk '{
+	n = split($0,pathname,":");
+	for( i = 1; i<=n; i++ )
+		print("Dir : "pathname[i]);
+	}'
+# -------------------------------------------------
