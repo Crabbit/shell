a293a65dfef5c2b618a2104c6219b8f3cc1ba69a
lili <lili00681@gmail.com>
2013-12-01 05:16:21 -0800

    新添加了在cu上看到的sed实现的一个例子

--- src/sed_awk/sed/sed_higher/tool.sh
+++ src/sed_awk/sed/sed_higher/tool.sh
@@ -11,9 +11,43 @@ h
 $p
 }' $1
 
+# 在每一行前面添加4个空格
+sed 's/^/    /' /etc/passwd
 
 # 添加空行
 sed 'G' /etc/passwd
 
+# 添加2行空行
+sed 'G;G' /etc/passwd
+
+# 在文本匹配后添加空行
+sed '/root/{G}' /etc/passwd
+
+# 在文本匹配前添加空行
+sed '/root/{x;p;x;}' /etc/passwd
+
+# 在文本匹配前后各添加一行
+sed '/root/{x;p;x;G}' /etc/passwd
+
 # 添加行号
 sed '=' /etc/passwd | sed 'N ; s/\n/ /'
+
+# 统计文件行数
+wc -n '$=' /etc/passwd
+
+# 将文本对其到80列
+# 这里&符号代表这行内容.
+# ^$就限制了文本长度
+# 第一次循环的时候，将会在前面补上第一个空格，以此类推
+sed '{
+:a
+s/^.\{1,79\}$/ &/
+t a
+}' /etc/passwd
+
+# 使所有文本处于80行的中央
+sed '{
+:a
+s/^.\{1,79\}$/ & /
+t a
+}' /etc/passwd
