385be9d182e6e6dfa479b623b6ac6312669e6a44
lili <lili00681@gmail.com>
2014-01-15 23:56:22 -0800

    对文件的一个简单处理

---
+++ src/sed_awk/gawk/gawk_higher/file.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Creat Time :Wed 15 Jan 2014 11:04:22 PM GMT
+# Autoor     : lili
+
+# 用来输出文件的一些信息
+echo ======================================================
+# ARGC  当前gawk处理行后面参数个数
+# ARGV  gawk处理参数数组
+# NF    当前数据行的字段总数
+# FNR   当前数据文件中的数据行数
+
+gawk '
+BEGIN{
+print "There is total  ",ARGC
+print "Options 0 :",ARGV[0]
+print "Options 1 :",ARGV[1]
+print "Options 2 :",ARGV[2]
+print "Options 3 :",ARGV[3]
+print "Options 4 :",ARGV[4]
+}' a b c d
+
+echo ======================================================
+gawk '
+BEGIN{
+print "The file is :",ARGV[1]
+}
+{print "Line ",FNR,$1,$NF
+}' /etc/passwd
